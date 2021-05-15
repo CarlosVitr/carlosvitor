7a69c1167da669bf8080e4538b6cd2cb804230db
Carlos Vítor <carlosvitorcampos@outlook.com>
2021-05-15 17:48:44 -0300

    feat: add index

---
+++ index.js
@@ -0,0 +1,23 @@
+{ 
+
+"name":
+  
+ "  
+          ______              __            
+         / ____/____ _ _____ / /____   _____
+        / /    / __ `// ___// // __ \ / ___/
+       / /___ / /_/ // /   / // /_/ /(__  ) 
+       \____/ \__,_//_/   /_/ \____//____/  
+            _    __ _  __                 
+           | |  / /(_)/ /_ ____   _____   
+           | | / // // __// __ \ / ___/   
+           | |/ // // /_ / /_/ // /       
+           |___//_/ \__/ \____//_/     
+   ",
+
+  "Website": "https://carlosvitor.ml/",
+  "E-mail": "carlosv.professional@gmail.com",
+  "Twitter": "https://twitter.com/ClsVitor",
+  "Github": "https://github.com/carlosvitr",
+  "LinkedIn": "https://linkedin.com/in/carlosvitor", 
+}
